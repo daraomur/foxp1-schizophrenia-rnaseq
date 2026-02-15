7a9452ab22715b84cd9de209b013411c0067a056
Dara O Murchu <149065181+daraomur@users.noreply.github.com>
2026-02-15 22:59:58 +0000

    Update deseq2_analysis.R

--- scripts/deseq2_analysis.R
+++ scripts/deseq2_analysis.R
@@ -15,11 +15,13 @@ quant_files <- unlist(lapply(subdirs, function(subdir) {
  
 # Print the files to check if they are correctly listed
 print(quant_files)
+
 # Check if the number of files is as expected
 if (length(quant_files) != 8) {
   stop("The number of quant.sf files found does not match the expected number (8). Check the directory and pattern.")
 }
- 
+
+# Define Sample Names
 sample_names <- c("SRR1630713","SRR1630714", "SRR1630715", "SRR1630716", "SRR1630717","SRR1630718","SRR1630719","SRR1630720")
  
 # Create a sample table
@@ -40,7 +42,7 @@ dds <- DESeqDataSetFromTximport(txi,
                                 colData = sample_table,
                                 design = ~ condition)
  
-# Pre-filtering (optional but recommended)
+# Pre-filtering
 dds <- dds[ rowSums(counts(dds)) > 1, ]
  
 # Run the DESeq2 pipeline
